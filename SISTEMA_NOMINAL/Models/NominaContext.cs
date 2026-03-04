fd47d4f783666b02207564164298cdba61eef1d6
David <David@DEIVLER575>
2026-03-04 01:05:30 -0500

    JBMH

---
+++ SISTEMA_NOMINAL/Models/NominaContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace SistemaNomina.Models
+{
+    public class NominaContext : DbContext
+    {
+        public NominaContext(DbContextOptions<NominaContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
--- SISTEMA_NOMINAL/Program.cs
+++ SISTEMA_NOMINAL/Program.cs
@@ -12,3 +12,4 @@ public class Program
                 webBuilder.UseStartup<Startup>();
             });
 }
+ 
